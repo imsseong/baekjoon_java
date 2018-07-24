638682dbeb3a083171aa238d8cde10cef0e2e33f
imsseong <hsung325@gmail.com>
2018-07-24 14:53:49 +0900

    baekjoon 10828 : 스택 https://www.acmicpc.net/problem/10828

---
+++ src/boj10828/Main.java
@@ -0,0 +1,36 @@
+package boj10828;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Stack<Integer>stack = new Stack<>();
+        String command;
+
+        for(int i = 0; i < n; i++) {
+            command = sc.next();
+
+            switch (command) {
+                case "push":
+                    int x = sc.nextInt();
+                    stack.push(x);
+                    break;
+                case "pop":
+                    System.out.println(stack.isEmpty() ? -1 : stack.pop());
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    System.out.println(stack.isEmpty() ? 1 : 0);
+                    break;
+                case "top":
+                    System.out.println(stack.isEmpty() ? -1 : stack.peek());
+                    break;
+            }
+        }
+    }
+}
