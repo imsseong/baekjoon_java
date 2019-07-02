d4ee4a331a2dc855e8c8d3ff8b30b9dcb972e48c
seongim <hsung325@gmail.com>
2019-07-02 14:10:32 +0900

    Add problem 1330
    baekjoon problem 1330 : 두 수 비교하기
    두 수를 비교한 결과를 출력하는 문제 해결

---
+++ src/boj1330/Main.java
@@ -0,0 +1,27 @@
+package boj1330;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		if(a > b) {
+			System.out.println(">");
+		} else if (a < b) {
+			System.out.println("<");
+		} else {
+			System.out.println("==");
+		}
+
+	} //main()
+
+}
