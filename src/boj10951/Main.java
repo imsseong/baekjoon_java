5ab384735999393668b83a63af6bb58a97f71891
seongim <hsung325@gmail.com>
2019-07-04 12:18:44 +0900

    Add problem 10951
    baekjoon problem 10951 : A+B -4
    입력이 끝날 때까지 A+B를 출력하는 문제해결. EOF에 대해 알아 보세요.
    참고) https://www.acmicpc.net/problem/10951

---
+++ src/boj10951/Main.java
@@ -0,0 +1,20 @@
+package boj10951;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			System.out.println(A + B);
+		}
+	} //main()
+}
